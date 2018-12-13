81dd7727223337b520f0f4dfe0805183e0c9a4a6
Dally Wally <dallin.watac.major@gmail.com>
2018-12-13 12:34:15 -0700

    user model

--- models/index.js
+++ models/index.js
@@ -1,1 +1,5 @@
 const User = require('./user');
+
+module.exports = {
+    User: User
+}
--- models/user.js
+++ models/user.js
@@ -1,24 +1,12 @@
 const UserSchema = new Schema ({
     username: {
         type: String,
-        required: true,
-        validate: [
-            function (input) {
-                return input.length >= 6;
-            },
-            "Username should be at least 6 characters long."
-        ]
+        trim: true,
+        required: true
     },
     password: {
         type: String,
-        trim: true,
-        required: "Password is Required",
-        validate: [
-            function (input) {
-                return input.length >= 6;
-            },
-            "Password should be longer."
-        ]
+        trim: true
     },
     date: {
         type: Date,
