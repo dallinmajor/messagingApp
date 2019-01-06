4fe9c3b8f0603396f3d82023b5743dda44160ee2
Dally Wally <dallin.watac.major@gmail.com>
2019-01-06 14:56:00 -0700

    trying to make it work

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 const router = require('express').Router();
 const APIroutes = require('./API');
-const path = require('path');
 
 router.use('/api', APIroutes);
-
-router.get('*', function (request, response) {
-  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,11 +5,27 @@ const numCPUs = require('os').cpus().length;
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const router = require('./routes');
+const Db = require('./config/key')
 
 const PORT = process.env.PORT || 5000;
+
+// Multi-process to utilize all CPU cores.
+if (cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
+
+} else {
   
   mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/mern_starter",
+    process.env.MONGODB_URI || Db,
     { useNewUrlParser: true }
   );
 
@@ -18,9 +34,13 @@ const PORT = process.env.PORT || 5000;
   app.use(bodyParser.json({ useNewUrlParser: true }));
   app.use(router);
 
-  // app.use(express.static(path.resolve(__dirname, '../client/build')));
+  app.use(express.static(path.resolve(__dirname, './client/build')));
+
+  app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  });
 
   app.listen(PORT, function () {
     console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
   });
-
+}
