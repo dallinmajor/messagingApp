15aa26b625670dc74b931bd58046eb7ac19a073f
Dally Wally <dallin.watac.major@gmail.com>
2019-01-06 11:27:04 -0700

    trying to get the build working for heroku

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,8 @@ const APIroutes = require('./API');
 
 router.use('/api', APIroutes);
 
-// router.get('/api', function (req, res) {
-//   res.set('Content-Type', 'application/json');
-//   res.send('{"message":"Hello from the custom server!"}');
-// });
-
-// router.get('*', function (request, response) {
-//   response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-// });
+router.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 module.exports = router;
