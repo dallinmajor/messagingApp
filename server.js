5c9002af626ba09ba976da9052ae3491550c8847
Dally Wally <dallin.watac.major@gmail.com>
2019-01-06 14:42:05 -0700

    heroku testing

--- server.js
+++ server.js
@@ -7,21 +7,6 @@ const mongoose = require('mongoose');
 const router = require('./routes');
 
 const PORT = process.env.PORT || 5000;
-
-// Multi-process to utilize all CPU cores.
-if (cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
-
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-  });
-
-} else {
   
   mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/mern_starter",
@@ -38,4 +23,4 @@ if (cluster.isMaster) {
   app.listen(PORT, function () {
     console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
   });
-}
+
