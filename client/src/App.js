6c86dcb05f1a2dfa326c2a13ca9a61b8b27ee749
Dallin Major <dallin.watac.major@gmail.com>
2019-05-02 14:49:11 -0600

    setup\

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from 'react';
 export default function App() {
   const [messages, setMessages] = useState([]);
 
+  
+
   return (
     <div>
       <h1>Hello World</h1>
